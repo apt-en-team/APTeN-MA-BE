1d00044774f518add537cf184f1712bd9994e687
kgee0503 <kgee0503@gmail.com>
2026-03-05 16:22:44 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/apt/BeAptenApplication.java
@@ -0,0 +1,13 @@
+package com.apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAptenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAptenApplication.class, args);
+    }
+
+}
