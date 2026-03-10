58eaee683055df40a9e32b180738131cd69229ee
soyoung <ssssy93@icloud.com>
2026-03-10 15:46:31 +0900

    merge: develop 충돌해결

---
+++ src/main/java/com/apt/household/dto/response/PageRes.java
@@ -0,0 +1,11 @@
+package com.apt.household.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PageRes {
+    private int maxPage;
+    private int totalCount;
+}
